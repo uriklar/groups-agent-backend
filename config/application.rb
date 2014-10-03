7389ad78f744eb370829362515c851eef5d77eba
Rubysoft <uriklar@gmail.com>
2014-10-03 19:18:59 +0300

    moved rack cors to config.ru

--- config/application.rb
+++ config/application.rb
@@ -9,12 +9,7 @@ Bundler.require(:default, Rails.env)
 module GroupsAgentBackend
   class Application < Rails::Application
 
-    config.middleware.insert_before "ActionDispatch::Static", "Rack::Cors" do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
